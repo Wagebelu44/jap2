0fa196556078091dc35b866a3a6e68fdf7dfdbdd
yaseen041 <yaseen3327095758@gmail.com>
2020-09-07 16:04:57 +0500

    Final commit

--- application/modules-backup-01-08-2019/common/views/admin_footer.php
+++
@@ -1,5 +0,0 @@
-<div class="footer">
-    <div>
-        <strong>Copyright</strong> JAP &copy; <?php echo date("Y"); ?>
-    </div>
-</div>
--- application/modules-backup-01-08-2019/home/views/change_gift_card_ajax.php
+++
@@ -1,7 +0,0 @@
-<div class="gift_card_img text-center text-white" id="change_gift_card">
-	<div class="double_img">
-		<img src="<?php echo base_url(); ?>assets/images/<?php echo $gift_cards['gift_card_value']?>.png" class="upper_image">
-		<img src="<?php echo base_url(); ?>assets/images/envelope.png" />
-	</div>
-	<p class="card_price"><?php echo CURRENCY.$gift_cards['actual_price']; ?></p>
-</div>
